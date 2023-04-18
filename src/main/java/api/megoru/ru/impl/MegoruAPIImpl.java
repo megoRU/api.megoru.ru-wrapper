d3ed63785406316be3b8d5ec8935dcb1569c217a
megoRU <savin.yura2014@gmail.com>
2023-04-18 14:54:12 +0300

    version 4.0
    1. Use now httpclient5

--- src/main/java/api/megoru/ru/impl/MegoruAPIImpl.java
+++ src/main/java/api/megoru/ru/impl/MegoruAPIImpl.java
@@ -8,17 +8,17 @@ import api.megoru.ru.io.UnsuccessfulHttpException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import okhttp3.HttpUrl;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -118,31 +118,37 @@ public class MegoruAPIImpl implements MegoruAPI {
         return execute(request, responseTransformer);
     }
 
-    private <E> E execute(HttpRequestBase request, ResponseTransformer<E> responseTransformer) throws UnsuccessfulHttpException, NullResponseException, IOException {
+    private <E> E execute(ClassicHttpRequest request, ResponseTransformer<E> responseTransformer) throws UnsuccessfulHttpException, IOException {
         CloseableHttpResponse response = httpClient.execute(request);
-        // Get HttpResponse Status
-        System.out.println("Status: " + response.getStatusLine().getStatusCode());
-        HttpEntity entity = response.getEntity();
 
-        if (entity == null) {
-            throw new NullResponseException();
-        }
-        String body = EntityUtils.toString(entity);
+        try {
+            // Get HttpResponse Status
+            System.out.println("Status: " + response.getCode());
+            HttpEntity entity = response.getEntity();
 
-        System.out.println(body);
-
-        switch (response.getStatusLine().getStatusCode()) {
-            case 401:
-            case 403:
-            case 404: {
-                ErrorResponse result = gson.fromJson(body, ErrorResponse.class);
-                throw new UnsuccessfulHttpException(result.getError().getCode(), result.getError().getMessage());
+            if (entity == null) {
+                throw new NullResponseException();
             }
-            case 200: {
-                return responseTransformer.transform(body);
+            String body = EntityUtils.toString(entity);
+
+            System.out.println(body);
+
+            switch (response.getCode()) {
+                case 401:
+                case 403:
+                case 404: {
+                    ErrorResponse result = gson.fromJson(body, ErrorResponse.class);
+                    throw new UnsuccessfulHttpException(result.getError().getCode(), result.getError().getMessage());
+                }
+                case 200: {
+                    return responseTransformer.transform(body);
+                }
+                default:
+                    throw new UnsuccessfulHttpException(response.getCode(), "API not work, or connection refused");
             }
-            default:
-                throw new UnsuccessfulHttpException(response.getStatusLine().getStatusCode(), "API not work, or connection refused");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new UnsuccessfulHttpException(response.getCode(), "API not work, or connection refused");
         }
     }
 }
